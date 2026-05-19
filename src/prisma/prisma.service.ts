7b4fb53b8b4baba057158621221ad87be31fbfbf
Jagadheeshwar Gurram <gurramjagadheeshwar@gmail.com~>
2026-05-19 17:20:52 +0530

    Fix Prisma SSL for Render

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -10,32 +10,18 @@ import { PrismaPg } from '@prisma/adapter-pg'
 
 import { Pool } from 'pg'
 
-import * as fs from 'fs'
-import * as path from 'path'
-
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit
 {
   constructor() {
-    const sslCert = fs.readFileSync(
-      path.join(
-        process.cwd(),
-        'certs',
-        'ca.pem',
-      ),
-      'utf8',
-    )
-
     const pool = new Pool({
       connectionString:
         process.env.DATABASE_URL,
 
       ssl: {
-        ca: sslCert,
-
-        rejectUnauthorized: true,
+        rejectUnauthorized: false,
       },
     })
 
